cc9577be836b1bcf3e640038bbd8120072357b4c
Travis Gorman <thefarbetween@me.com>
2016-12-12 23:14:59 -0600

    some css

--- app/components/App.js
+++ app/components/App.js
@@ -9,21 +9,23 @@ const App = React.createClass({
     };
   },
   styles: {
-    'fontFamily':'sans-serif','textAlign':'center', 'marginTop':'4rem'
+    'fontFamily':'sans-serif',
+    'textAlign':'center',
+    'marginTop':'4rem'
   },
   render(){
     return (
       <div>
         <div style={this.styles}>
           <h3>The status of this app is:</h3>
-          <h1 style={{'color':'green'}}>
+          <h1 style={{ 'color':'green','fontSize':46 }}>
             {this.state.status}
           </h1>
         </div>
         <Message 
-            title="Swiggity Swooty" 
-            message="I'm comin' for that booty!"
-            src='http://www.deathmetal.org/wp-content/uploads/trump-two-thumbs-up.jpg' />
+          title="Swiggity Swooty" 
+          message="I'm comin' for that booty!"
+          src='http://www.deathmetal.org/wp-content/uploads/trump-two-thumbs-up.jpg' />
       </div>
       
     );
